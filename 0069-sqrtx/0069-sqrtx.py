265b4fa636cc16a86708b0e825dafd93e0d0df9a
ashutosh454 <ashutoshpratapmaurya24@gmail.com>
2026-07-15 05:47:05 +0530

    Time: 7 ms (30.06%), Space: 19.4 MB (23.31%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r=1,x
+        ans=0
+        while l<=r:
+            mid=(l+r)//2
+            if mid*mid>x:
+                r=mid-1
+            else:
+                ans=mid
+                l=mid+1
+        return ans
