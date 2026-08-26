bdeaa4f7828d53c2ca94a8d0be1c4cc20e27f121
ashutosh454 <ashutoshpratapmaurya24@gmail.com>
2026-08-26 10:47:01 +0530

    Time: 69 ms (23.05%), Space: 20.9 MB (48.33%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        result=[]
+
+        def generate(curr_str: str):
+            if len(curr_str) == n:
+                result.append(curr_str)
+                return
+
+            generate(curr_str + "1")
+
+            if not curr_str or curr_str[-1] == "1":
+                generate(curr_str + "0")
+
+        generate("0")
+        generate("1")
+        return result
+
