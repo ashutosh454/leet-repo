57d73b36aefa084d71fcecb75524bbe8d79b45c7
ashutosh454 <ashutoshpratapmaurya24@gmail.com>
2026-09-20 19:10:37 +0530

    Time: 248 ms (45.91%), Space: 21.6 MB (73.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,28 @@
 class Solution:
-    def search(self,r,c,visited,grid):
-        rows = len(grid)
-        cols = len(grid[0])
-        
-        if r<0 or r == rows or c<0 or c == cols:
-            return 
-        if visited[r][c] == 1 or grid[r][c] == '0':
-            return
-        visited[r][c] = 1
-        self.search(r+1,c,visited,grid)
-        self.search(r,c+1,visited,grid)
-        self.search(r,c-1,visited,grid)
-        self.search(r-1,c,visited,grid)
-
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
         rows = len(grid)
         cols = len(grid[0])
         count = 0
-        visited = [[0 for _ in range(cols)] for _ in range(rows)]
-        if not grid:
-            return 0
+
+        def helper(r,c):
+            if r < 0 or r == rows or c < 0 or c == cols:
+                return
+            if grid[r][c] == '0':
+                return
+            
+            grid[r][c] = '0'
+
+            helper(r+1,c)
+            helper(r,c+1)
+            helper(r,c-1)
+            helper(r-1,c)
+
         for r in range(rows):
             for c in range(cols):
-                if visited[r][c] == 0 and grid[r][c] == '1':
-                    self.search(r,c,visited,grid)
-                    count+=1
-        
+                if grid[r][c] == '1':
+                    count +=1
+                    helper(r,c)
+                    
         return count
-
-
-
-
-        
