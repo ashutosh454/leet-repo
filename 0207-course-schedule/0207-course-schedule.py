f8ce4fcd6606a5537919f359f56d41e268f389f4
ashutosh454 <ashutoshpratapmaurya24@gmail.com>
2026-09-23 11:30:57 +0530

    Time: 3 ms (85.85%), Space: 20.5 MB (62.97%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        indegree = [0 for _ in range(numCourses)]
+        adj_list = [[] for _ in range(numCourses)]
+
+        for u,v in prerequisites:
+            adj_list[v].append(u)
+            indegree[u]+=1
+        
+        queue = deque()
+        count = 0
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        while queue:
+            current_node = queue.popleft()
+            count +=1
+
+            for adjNode in adj_list[current_node]:
+                indegree[adjNode] -= 1
+                if indegree[adjNode] == 0:
+                    queue.append(adjNode)
+        
+        return count == numCourses
+
